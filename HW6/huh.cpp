91431ef3572cd303827476b6dbb629b5029c3fc7
Ibrahim Urooj Saeed <ibrahim.urooj@gmail.com>
2014-06-02 12:03:52 -0400

    first commit

---
+++ HW6/huh.cpp
@@ -0,0 +1,9 @@
+//
+//  huh.cpp
+//  HW6
+//
+//  Created by Ibrahim Saeed on 12/6/12.
+//  Copyright (c) 2012 Ibrahim Saeed. All rights reserved.
+//
+
+#include "huh.h"
---
+++ HW6/huh.h
@@ -0,0 +1,14 @@
+//
+//  huh.h
+//  HW6
+//
+//  Created by Ibrahim Saeed on 12/6/12.
+//  Copyright (c) 2012 Ibrahim Saeed. All rights reserved.
+//
+
+#ifndef __HW6__huh__
+#define __HW6__huh__
+
+#include <iostream>
+
+#endif /* defined(__HW6__huh__) */
